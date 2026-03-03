b088509e5ff80ff17a780d3ce199f785dc9bbb8e
Adam Myers <adammyers002@gmail.com>
2026-03-03 14:24:24 -0600

    add course CRUD views for planner

--- studyplanner/planner/models.py
+++ studyplanner/planner/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -11,3 +12,6 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("course-detail", kwargs={"pk": self.id})
--- studyplanner/planner/views.py
+++ studyplanner/planner/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.urls import reverse_lazy
+from .models import Course
+
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+
+class CourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = "courses/index.html"
+    context_object_name = "courses"
+
+    def get_queryset(self):
+        return Course.objects.filter(user=self.request.user).order_by("name")
+
+
+class CourseDetailView(LoginRequiredMixin, DetailView):
+    model = Course
+    template_name = "courses/detail.html"
+
+    def get_queryset(self):
+        return Course.objects.filter(user=self.request.user)
+
+
+class CourseCreateView(LoginRequiredMixin, CreateView):
+    model = Course
+    fields = ["name", "instructor", "color"]
+    template_name = "courses/form.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class CourseUpdateView(LoginRequiredMixin, UpdateView):
+    model = Course
+    fields = ["name", "instructor", "color"]
+    template_name = "courses/form.html"
+
+    def get_queryset(self):
+        return Course.objects.filter(user=self.request.user)
+
+
+class CourseDeleteView(LoginRequiredMixin, DeleteView):
+    model = Course
+    template_name = "courses/confirm_delete.html"
+    success_url = reverse_lazy("course-list")
+
+    def get_queryset(self):
+        return Course.objects.filter(user=self.request.user)
